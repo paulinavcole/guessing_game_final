0e7a5121d2634f5784df1faa1ab4998ed1968ee1
Paulina Cole <lina.cole@gmail.com>
2022-04-12 19:15:55 -0400

    updated game functionality

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,14 +1,6 @@
-/* 
 
-Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
-by running "testem".
 
-In this file, you will also include the event listeners that are needed to interact with your HTML file when
-a user clicks a button or adds a guess to the input field.
-
-*/
-
-//document.getElementById('');
+document.getElementById('');
 
 function generateWinningNumber() {
     let number = Math.floor(Math.random() * 100) + 1
@@ -77,8 +69,8 @@ function shuffle(array) {
                 else feedbackText = "You're ice cold!"
             }
            }
-           //document.querySelector('#guess-feedback > h4').innerHTML = feedbackText;
-           //document.querySelector(`#guess-list li:nth-child(${this.pastGuesses.length})`).innerHTML = this.playersGuess
+           document.querySelector('#guess-feedback > h4').innerHTML = feedbackText;
+           document.querySelector(`#guess-list li:nth-child(${this.pastGuesses.length})`).innerHTML = this.playersGuess
            
         return feedbackText
       }
@@ -97,16 +89,16 @@ function shuffle(array) {
     const game = new Game();
     
     // We are grabbing the button from our html
-    //const button = document.querySelector('button');
+    const button = document.querySelector('button');
   
     // We are listening for when the use clicks on our button.
     // When they click, we will check in the input field to see if they have guessed a number. Then we will run the function `checkGuess`, and give it the player's guess, the winning number, and the empty array of guesses!
-    //button.addEventListener('click', function() {
-      //const playersGuess = +document.querySelector('input').value;
-      //document.querySelector('input').value = '';
+     button.addEventListener('click', function() {
+      const playersGuess = +document.querySelector('input').value;
+      document.querySelector('input').value = '';
   
-      //game.playersGuessSubmission(playersGuess);
-    //});
+      game.playersGuessSubmission(playersGuess);
+    });
 
     return game
   }
