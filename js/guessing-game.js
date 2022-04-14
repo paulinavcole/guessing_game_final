853f701f6a7fc0b6eb573d9e0c432387026d4524
Paulina Cole <lina.cole@gmail.com>
2022-04-13 21:03:11 -0400

    added reset game button with functionality

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -54,13 +54,13 @@ function shuffle(array) {
 
         let feedbackText = ''
           if(this.playersGuess === this.winningNumber) {
-              feedbackText = 'You Win!'
+              feedbackText = 'You Win! Reset the game to play again!'
           } else if (this.pastGuesses.includes(this.playersGuess)) {
               feedbackText = 'You have already guessed that number.'
           } else {
             this.pastGuesses.push(this.playersGuess)
             if (this.pastGuesses.length === 5) {
-            feedbackText = 'You Lose.'
+            feedbackText = 'You Lose. Reset the game to play again!'
            } else {
                let difference = this.difference()
                if (difference < 10) feedbackText = "You're burning up!"
@@ -80,28 +80,39 @@ function shuffle(array) {
           hintArray.push(generateWinningNumber(), generateWinningNumber(), this.winningNumber)
           shuffle(hintArray)
           return hintArray
-      }
+      
+      }   
   }
+
   function newGame() {
     return new Game(); //check that old game !== new game
   }
+
+
   function playGame() {
+
     const game = new Game();
     
     // We are grabbing the button from our html
-    const button = document.querySelector('button');
+    const button = document.getElementById('submit');
   
     // We are listening for when the use clicks on our button.
     // When they click, we will check in the input field to see if they have guessed a number. Then we will run the function `checkGuess`, and give it the player's guess, the winning number, and the empty array of guesses!
      button.addEventListener('click', function() {
       const playersGuess = +document.querySelector('input').value;
       document.querySelector('input').value = '';
-  
       game.playersGuessSubmission(playersGuess);
+
     });
+
 
     return game
   }
 
+  function reset() {
+    location.reload()
+  }
+
   playGame();
+
 
